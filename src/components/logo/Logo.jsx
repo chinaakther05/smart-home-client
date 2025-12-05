b86c43d0757b176f2acd681e2c0803ea0198d670
Your Namesohaggaji07@gmail.com <sohaggaji07@gmail.com>
2025-12-06 02:38:20 +0600

    first commit

---
+++ src/components/logo/Logo.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import logo from '../../assets/home.png'
+
+const Logo = () => {
+    return (
+        <div className='flex items-center space-x-3'>
+    <img 
+        className='w-[70px] h-[70px] rounded-full object-cover' 
+        src={logo} 
+        alt="Smart Home Logo" 
+    />
+    <h2 className='text-3xl font-bold italic '> <span className='text-pink-400 italic'>Smart</span>
+  <span className='text-4xl text-green-400 italic'>H</span>ome
+</h2>
+
+</div>
+
+
+    );
+};
+
+export default Logo;
